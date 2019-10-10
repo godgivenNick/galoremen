04b073632a518e29a1cfb141526302072644afe5
nick_gg <godgiven.nick@gmail.com>
2019-10-10 17:47:34 +0300

    adaptive 7/12

--- build/js/brands.js
+++ build/js/brands.js
@@ -1,24 +1,32 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    var container = document.querySelector('.brands-list__n-container');
-    var items = Array.from(container.querySelectorAll('.brands-list-item'));
+    var client_width = document.documentElement.clientWidth;
 
-    var ost_3 = +items.length % 3;
-    // console.log(items.length);
-    // console.log(ost_3);
+    if(+client_width > 1024){
 
-    var default_item_html = `
-      <div class="brands-list-item _empty">
-        <div class="x-mark-160"></div>
-      </div>
-    `;
+      var container = document.querySelector('.brands-list__n-container');
+      var items = Array.from(container.querySelectorAll('.brands-list-item'));
+  
+      var ost_3 = +items.length % 3;
+      // console.log(items.length);
+      // console.log(ost_3);
+  
+      var default_item_html = `
+        <div class="brands-list-item _empty">
+          <div class="x-mark-160"></div>
+        </div>
+      `;
+  
+      if(ost_3 == 1){
+          container.innerHTML += default_item_html;
+          container.innerHTML += default_item_html;
+      } else if (ost_3 == 2){
+          container.innerHTML += default_item_html;
+      }
 
-    if(ost_3 == 1){
-        container.innerHTML += default_item_html;
-        container.innerHTML += default_item_html;
-    } else if (ost_3 == 2){
-        container.innerHTML += default_item_html;
+      
     }
+
 
 
 });
--- build/js/main.js
+++ build/js/main.js
@@ -33,4 +33,41 @@ document.addEventListener('DOMContentLoaded', function(){
     
     });
 
+
+    var main_promo_slider = new Swiper('.main-first', {
+    
+        init: true,
+        slidesPerView: 1,
+        speed: 1000,
+        spaceBetween: 0,
+    
+        loop: true,
+        autoplay: {
+            delay: 8000,
+        },
+    
+        navigation: {
+            prevEl: '[prev]',
+            nextEl: '[next]',
+        },
+    
+    });
+
+    main_promo_slider.on('slideChange', function () {
+
+        var current_counter = document.querySelector('.main-first__counter--current');
+
+        setTimeout(function(){
+
+            var prev_count = document.querySelector('.swiper-slide-active').getAttribute('counter');
+            current_counter.innerHTML = prev_count;
+            console.log(prev_count)
+
+        }, 5);
+
+    });
+
+
+
+
 });
